ffeaa048755e83973b10f0f0b26c59960c812556
Mihika Dakappagari <mihikashrinivas@gmail.com>
2025-11-09 01:35:55 -0800

    Update

--- sugarbooth/vite.config.ts
+++ sugarbooth/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SugarBooth/', // <-- repo name, with leading/trailing slash
 })
